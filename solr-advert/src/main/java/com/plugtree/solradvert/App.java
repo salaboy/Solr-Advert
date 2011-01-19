aa4bf2e7d10314bc0c86fedd2cdff52eaa7d5028
Salaboy <salaboy@gmail.com>
2011-01-19 10:27:23 -0300

    initial commit

---
+++ solr-advert/src/main/java/com/plugtree/solradvert/App.java
@@ -0,0 +1,13 @@
+package com.plugtree.solradvert;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
